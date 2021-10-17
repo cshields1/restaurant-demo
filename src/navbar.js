e1bec17ac1112d3d75e01808021070c9f42a4cbb
Craig <84283410+cshields1@users.noreply.github.com>
2021-10-17 15:01:37 -0700

    Initial navbar

---
+++ src/navbar.js
@@ -0,0 +1,31 @@
+import loadHomepage from "./homepage";
+import loadMenu from "./menu";
+import loadContact from "./contact";
+
+const loadNavbar = () => {
+  const display = document.querySelector("#content");
+
+  const buttonContainer = document.createElement("div");
+  buttonContainer.id = "btn-container";
+
+  const homeButton = document.createElement("button");
+  homeButton.classList.add("home-btn");
+  homeButton.textContent = "Home";
+  homeButton.addEventListener("click", loadHomepage);
+
+  const menuButton = document.createElement("button");
+  menuButton.classList.add("menu-btn");
+  menuButton.textContent = "Menu";
+  menuButton.addEventListener("click", loadMenu);
+
+  const contactButton = document.createElement("button");
+  contactButton.classList.add("contact-btn");
+  contactButton.textContent = "Contact";
+  contactButton.addEventListener("click", loadContact);
+
+  buttonContainer.append(homeButton, menuButton, contactButton);
+
+  display.append(buttonContainer);
+};
+
+export default loadNavbar;
