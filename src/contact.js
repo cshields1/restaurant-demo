59d258a0fe9a1bec1a025f1b58159adf19d7ec45
Craig <84283410+cshields1@users.noreply.github.com>
2021-10-17 15:02:16 -0700

    Clear display and add info

--- src/contact.js
+++ src/contact.js
@@ -1,19 +1,28 @@
-const loadContactTab = () => {
+import loadNavbar from "./navbar";
+
+const loadContact = () => {
+  loadNavbar();
+
   const display = document.querySelector("#content");
+
+  while (display.firstChild) {
+    display.firstChild.remove();
+  }
 
   const contactImage = new Image();
   contactImage.src = "";
-  display.append(contactImage);
 
   const contactContainer = document.createElement("section");
-  contactContainer.classList.add("contact-div");
+  contactContainer.id = "contact-div";
+
   const contactInfo = document.createElement("p");
   contactInfo.classList.add("contact-info");
   contactInfo.innerHTML =
-    "<h2>Address</h2><p>address</p><h2>Phone Number</h2><p>phone</p><h2>Email</h2><p>email</p>";
+    "<h2>Address</h2><p>1406 S. Fairview St.<br>Santa Ana, CA 92704</p><h2>Phone Number</h2><p>(714) 435-7857</p><h2>Email</h2><p><a href='mailto:dale@chughut.com'>dale@chughut.com</a></p>";
 
   contactContainer.append(contactInfo);
-  display.append(contactContainer);
+
+  display.append(contactImage, contactContainer);
 };
 
-export default loadContactTab;
+export default loadContact;
