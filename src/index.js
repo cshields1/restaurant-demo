b0f97708f8e098311ca6f05e15183bc088bf9d82
Craig <84283410+cshields1@users.noreply.github.com>
2021-10-17 15:02:53 -0700

    Remove navbar

--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
 import loadHomepage from "./homepage";
-import loadContactTab from "./contact";
 
 loadHomepage();
-
-const homeButton = document.createElement("button");
-homeButton.classList.add("home-btn");
-
-const contactButton = document.createElement("button");
-contactButton.classList.add("contact-btn");
-contactButton.addEventListener("click", loadContactTab);
