d4db7cea854fea4560fd28be57f7ff1823b9dd56
Craig <84283410+cshields1@users.noreply.github.com>
2021-10-17 12:30:04 -0700

    Basic contact tab structure

--- src/contact.js
+++ src/contact.js
@@ -1,3 +1,19 @@
-const loadContact = () => {};
+const loadContactTab = () => {
+  const display = document.querySelector("#content");
 
-export default loadContact;
+  const contactImage = new Image();
+  contactImage.src = "";
+  display.append(contactImage);
+
+  const contactContainer = document.createElement("section");
+  contactContainer.classList.add("contact-div");
+  const contactInfo = document.createElement("p");
+  contactInfo.classList.add("contact-info");
+  contactInfo.innerHTML =
+    "<h2>Address</h2><p>address</p><h2>Phone Number</h2><p>phone</p><h2>Email</h2><p>email</p>";
+
+  contactContainer.append(contactInfo);
+  display.append(contactContainer);
+};
+
+export default loadContactTab;
