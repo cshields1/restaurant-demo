4b552b22e16c81c4686dcc02b0c37509da795065
Craig <84283410+cshields1@users.noreply.github.com>
2021-10-17 15:02:38 -0700

    Add navbar

--- src/homepage.js
+++ src/homepage.js
@@ -1,20 +1,22 @@
 import "./style.css";
+import loadNavbar from "./navbar";
 
 const loadHomepage = () => {
+  loadNavbar();
+
   const display = document.querySelector("#content");
 
   const heroImage = new Image();
   heroImage.src = "../assets/dale-desk.jpeg";
-  display.append(heroImage);
 
   const headline = document.createElement("h1");
   headline.textContent = "Chug Hut";
-  display.append(headline);
 
   const copy = document.createElement("p");
   copy.textContent =
     "Chef Dale is hard at work making sure that the pizzas, pancakes, and pies at Chug Hut are sourced from Orange County's finest vendors.";
-  display.append(copy);
+
+  display.append(heroImage, headline, copy);
 };
 
 export default loadHomepage;
