1adec77cdba165ea33db506e7cf3f5d4241ec31b
Craig <84283410+cshields1@users.noreply.github.com>
2021-10-22 22:47:58 -0700

    Kill innerHTML

--- src/menu.js
+++ src/menu.js
@@ -1,4 +1,8 @@
 import loadNavbar from "./navbar";
+import PeanutButterBaconBites from '../assets/peanut-butter-bacon-bites.jpeg';
+import TunaAppleSalad from '../assets/tuna-apple-salad.jpeg';
+import CheesyChickenPizza from '../assets/cheesy-chicken-pizza.jpeg';
+import PeanutButterBananaIceCream from '../assets/peanut-butter-banana-ice-cream.jpeg';
 
 const loadMenu = () => {
   loadNavbar();
@@ -9,21 +13,45 @@ const loadMenu = () => {
   menu.id = "menu"
 
   const snack = document.createElement('div');
-  snack.innerHTML = "<h2>Peanut Butter Bacon Bites</h2><img src='../assets/peanut-butter-bacon-bites.jpeg'><p>Salt and umami collide in these chewy, hot-out-of-the-oven delights.</p>";
+  const snackName = document.createElement('h2');
+  snackName.textContent = "Peanut Butter Bacon Bites";
+  const snackImage = new Image();
+  snackImage.src = PeanutButterBaconBites;
+  const snackInfo = document.createElement('p');
+  snackInfo.textContent = "Salt and umami collide in these chewy, hot-out-of-the-oven delights.";
+  snack.append(snackName, snackImage, snackInfo);
 
   const salad = document.createElement('div');
-  salad.innerHTML = "<h2>Tuna and Apple Refresh</h2><img src='../assets/tuna-apple-salad.jpeg'><p>Add the zest your day is missing beside the comfort of an old favorite.</p>";
+  const saladName = document.createElement('h2');
+  saladName.textContent = "Tuna and Apple Refresh";
+  const saladImage = new Image();
+  saladImage.src = TunaAppleSalad;
+  const saladInfo = document.createElement('p');
+  saladInfo.textContent = "Add the zest your day is missing beside the comfort of an old favorite.";
+  salad.append(saladName, saladImage, saladInfo);
 
   const pizza = document.createElement('div');
-  pizza.innerHTML = "<h2>Cheesy Chicken Deluxe</h2><img src='../assets/cheesy-chicken-pizza.jpeg'><p>Our signature pie, topped with juicy chicken and mountains of melted cheese.</p>";
+  const pizzaName = document.createElement('h2');
+  pizzaName.textContent = "Cheesy Chicken Deluxe";
+  const pizzaImage = new Image();
+  pizzaImage.src = CheesyChickenPizza;
+  const pizzaInfo = document.createElement('p');
+  pizzaInfo.textContent = "Our signature pie, topped with juicy chicken and mountains of melted cheese.";
+  pizza.append(pizzaName, pizzaImage, pizzaInfo);
 
   const dessert = document.createElement('div');
-  dessert.innerHTML = "<h2>Chuggy Freeze</h2><img src='../assets/peanut-butter-banana-ice-cream.jpeg'><p>The perfect topper on any party. Ask about the birthday cup!</p>";
+  const dessertName = document.createElement('h2');
+  dessertName.textContent = "Chuggy Freeze";
+  const dessertImage = new Image();
+  dessertImage.src = PeanutButterBananaIceCream;
+  const dessertInfo = document.createElement('p');
+  dessertInfo.textContent = "The perfect topper on any party. Ask about the birthday cup!";
+  dessert.append(dessertName, dessertImage, dessertInfo);
 
   const signature = document.createElement('span')
   signature.innerHTML = "Made by Craig Shields for <a href='www.theodinproject.com'>The Odin Project</a>."
 
-  menu.append(snack,salad,pizza,dessert, signature);
+  menu.append(snack,salad,pizza,dessert,signature);
 
   display.append(menu);
 };
