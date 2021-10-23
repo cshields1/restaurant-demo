17bc7c7e166ecb8e57757ffb813aa2ba106abea6
Craig <84283410+cshields1@users.noreply.github.com>
2021-10-22 21:10:55 -0700

    Add descriptions and signature

--- src/menu.js
+++ src/menu.js
@@ -9,18 +9,21 @@ const loadMenu = () => {
   menu.id = "menu"
 
   const snack = document.createElement('div');
-  snack.innerHTML = "<h2>Peanut Butter Bacon Bites</h2><img src='../assets/peanut-butter-bacon-bites.jpeg'><p>Peanut Butter Bacon Bites stuff</p>";
+  snack.innerHTML = "<h2>Peanut Butter Bacon Bites</h2><img src='../assets/peanut-butter-bacon-bites.jpeg'><p>Salt and umami collide in these chewy, hot-out-of-the-oven delights.</p>";
 
   const salad = document.createElement('div');
-  salad.innerHTML = "<h2>Tuna and Apple Refresh</h2><img src='../assets/tuna-apple-salad.jpeg'><p>Tuna and Apple stuff</p>";
+  salad.innerHTML = "<h2>Tuna and Apple Refresh</h2><img src='../assets/tuna-apple-salad.jpeg'><p>Add the zest your day is missing beside the comfort of an old favorite.</p>";
 
   const pizza = document.createElement('div');
-  pizza.innerHTML = "<h2>Cheesy Chicken Deluxe</h2><img src='../assets/cheesy-chicken-pizza.jpeg'><p>Cheesy Chicken Deluxe stuff</p>";
+  pizza.innerHTML = "<h2>Cheesy Chicken Deluxe</h2><img src='../assets/cheesy-chicken-pizza.jpeg'><p>Our signature pie, topped with juicy chicken and mountains of melted cheese.</p>";
 
   const dessert = document.createElement('div');
-  dessert.innerHTML = "<h2>Chuggy Freeze</h2><img src='../assets/peanut-butter-banana-ice-cream.jpeg'><p>Peanut Butter Bacon Bites stuff</p>";
+  dessert.innerHTML = "<h2>Chuggy Freeze</h2><img src='../assets/peanut-butter-banana-ice-cream.jpeg'><p>The perfect topper on any party. Ask about the birthday cup!</p>";
 
-  menu.append(snack,salad,pizza,dessert);
+  const signature = document.createElement('span')
+  signature.innerHTML = "Made by Craig Shields for <a href='www.theodinproject.com'>The Odin Project</a>."
+
+  menu.append(snack,salad,pizza,dessert, signature);
 
   display.append(menu);
 };
